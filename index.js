8316cf4626892cf11007c9c0077a1d965c64dfc1
wigzen <wigzen18@gmail.com>
2023-01-28 11:50:09 +0530

    Frontend

---
+++ index.js
@@ -0,0 +1,109 @@
+let login = document.getElementById("login");
+let emailToLogin = document.getElementById("emailToLogin");
+let sigin = document.getElementById("sigin");
+let loginWithEmail = document.getElementById("loginWithEmail");
+
+const ourItemDiv = document.getElementsByClassName("item");
+const openIcon = document.getElementsByClassName("iconOpen");
+const closeIcon = document.getElementsByClassName("iconClose");
+
+let loginSection = document.getElementsByClassName("login")[0];
+let signupSection = document.getElementsByClassName("signup")[0];
+let signinSection = document.getElementsByClassName("signin")[0];
+               
+const loginForm = document.getElementById("login__form")
+
+login.addEventListener("click", () => {
+  loginSection.style.display = "none";
+  signupSection.style.display = "grid";
+});
+
+// emailToLogin.addEventListener("click", () => {
+//   signinSection.style.display = "none";
+//   signupSection.style.display = "grid";
+// });
+sigin.addEventListener("click", () => {
+  loginSection.style.display = "grid";
+  signupSection.style.display = "none";
+});
+
+// allOptions.addEventListener("click", () => {
+//   loginSection.style.display = "grid";
+//   signinSection.style.display = "none";
+// });
+
+// loginWithEmail.addEventListener('click',()=>{
+//     signinSection.style.display = "grid";
+//     loginSection.style.display = "none";
+
+// })
+
+// Compare password START
+let signupPassword = document.getElementById("signup-password");
+let signupConfirmpassword = document.getElementById("signup-confirmPassword");
+
+function validatePassword() {
+  if (signupPassword.value != signupConfirmpassword.value) {
+    signupConfirmpassword.setCustomValidity("Passwords Don't Match");
+  } else {
+    signupConfirmpassword.setCustomValidity("");
+  }
+}
+
+signupPassword.onchange = validatePassword;
+signupConfirmpassword.onchange = validatePassword;
+// Compare password END
+
+// show password START
+const [passwordIconOne, passwordIconTwo] =
+  document.getElementsByClassName("showPassword");
+
+passwordIconOne.addEventListener("click", () => {
+
+  let icon = document.getElementsByClassName("showPassword--close")[0];
+  if (icon.style.display === "block" && signupPassword.type=="text") {
+    icon.style.display = "none";
+    signupPassword.type = "password";
+  } else {
+    icon.style.display = "block";
+    signupPassword.type = "text";
+  }
+});
+
+passwordIconTwo.addEventListener("click", () => {
+
+  let icon = document.getElementsByClassName("showPassword--close")[1];
+  if (icon.style.display === "block" && signupConfirmpassword.type=="text") {
+    icon.style.display = "none";
+    signupConfirmpassword.type = "password";
+  } else {
+    icon.style.display = "block";
+    signupConfirmpassword.type = "text";
+  }
+});
+
+
+// show password END
+
+// Login START
+
+loginForm.addEventListener('submit',(e)=>{
+e.preventDefault();
+const dataForm = new FormData(loginForm);
+const res=Object.fromEntries(dataForm)
+console.log(JSON.stringify(res))
+})
+// Login END 
+
+// signup Form START
+const  signupForm = document.getElementById("signup__form")
+
+console.log(signupForm)
+signupForm.addEventListener('submit',(e)=>{
+  console.log("AA")
+  e.preventDefault();
+  const dataForm = new FormData(signupForm);
+  const res=Object.fromEntries(dataForm)
+  console.log(JSON.stringify(res))
+  })
+// signup Form END
---
+++ tools.js
@@ -0,0 +1,119 @@
+// const items = document.querySelectorAll(".accordion button");
+
+// function toggleAccordion() {
+//   const itemToggle = this.getAttribute("aria-expanded");
+
+//   for (let i = 0; i < items.length; i++) {
+//     items[i].setAttribute("aria-expanded", "false");
+//   }
+
+//   if (itemToggle == "false") {
+//     this.setAttribute("aria-expanded", "true");
+//     // api call
+//   }
+// }
+// items.forEach((item) => item.addEventListener("click", toggleAccordion));
+const textSubmit = document.getElementById("textSubmit");
+const textDiv = document.getElementsByClassName("accordion")[0];
+const form = document.getElementById("suggestedForm");
+const inputText = document.getElementById("textArea");
+let apiResponse = ["good", "average", "bad"];
+
+
+
+textSubmit.addEventListener("click", () => {
+  
+  let copy = inputText.value;
+  let textArray = copy.split(".");
+
+  textArray.forEach((element) => {
+    const userDiv = document.createElement("div");
+    userDiv.classList.add("accordion-item");
+    // console.log(userDiv);
+    userDiv.innerHTML = `
+    <button 
+    id="accordion-button" aria-expanded="false" class="title-wrapper"  data-loaded="false">
+    <span class="accordion-title"
+    ><p class="accordion-title-match">
+    ${element}
+    </p>
+    <div class="scores">
+ 12
+    </div>
+    </span>
+    
+    <span class="icon" aria-hidden="true"></span>
+    </button>
+    <div class="accordion-content">
+    <ul class="accordion-content--list">
+    </ul>
+    </div>
+    `;
+
+    textDiv.appendChild(userDiv);
+  });
+});
+
+
+function secondToggleAccordion(target) {
+  const name =target.querySelector(".accordion-title-match").innerText
+  console.log(name)
+    if (target.getAttribute("aria-expanded") != "true") {
+      // console.log(target.getAttribute("aria-expanded"));
+      const isloaded = target.getAttribute("data-loaded");
+      target.setAttribute("aria-expanded", true);
+    
+  
+      const content = target.parentNode.children[1].children[0];
+      console.log(content);
+      if (isloaded == "false") {
+        apiResponse.forEach((element) => {
+          // console.log(temp);
+  
+          content.insertAdjacentHTML(
+            "beforeend",
+            `
+    <li class="flex">
+    <label
+      >${element}</label
+    >
+    <div class="flex">
+      <div class="scores">
+10
+      </div>
+      <input type="radio" value="${element}" name="${name}" />
+    </div>
+  </li>
+    `
+          );
+        });
+        target.setAttribute("data-loaded", "true");
+      }
+    } else {
+      console.log("hehe");
+      target.setAttribute("aria-expanded", false);
+    }
+  }
+  
+  
+textDiv.addEventListener("click", (e) => {
+  let target = e.target;
+
+  while (target != this) {
+    if (target.tagName == "BUTTON") {
+      secondToggleAccordion(target);
+      //   toggleAccordion()
+      return;
+    }
+    target = target.parentNode;
+  }
+});
+
+
+form.addEventListener('submit',(e)=>{
+  console.log(form.children)
+e.preventDefault()
+  let data = new FormData(form)
+  let res = Object.entries(data)
+  console.log(res,data.keys())
+})

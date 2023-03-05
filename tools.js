fc8ae00f9e26ba122b47c09f13f9b1cd2a666268
wigzen <wigzen18@gmail.com>
2023-03-05 15:07:07 +0530

    New feature

--- tools.js
+++ tools.js
@@ -4,10 +4,14 @@ const form = document.getElementById("suggestedForm");
 const inputText = document.getElementById("textArea");
 const avatatBtn = document.getElementById("avatar__img");
 const final__form =document.getElementById('final__form')
-const apiResponse = ["good", "average", "bad"];
+const apiResponse = ["It's not wewewewewe writers who can benefit from this free online tool", " If you're a programmer who's working on a project where blocks of text are neeeeeeeeeeeeeeded, this tool can be a great way to get that", "It's a good way to test your programming and that the tool being created is working welllllll"];
 const improve = document.getElementById("improve")
+const textDump = document.getElementsByClassName("textDump")[0]
+const template = document.getElementById('template')
+const textContianer =  template.content.cloneNode(true)
 
 
+console.log(textContianer)
 let textArray =[];
 textSubmit.addEventListener("click", (e) => {
   // console.log(textDiv)
@@ -18,29 +22,23 @@ textSubmit.addEventListener("click", (e) => {
   textArray.forEach((element) => {
     if(element.length>0){
 
-    
     const userDiv = document.createElement("div");
+    
     userDiv.classList.add("accordion-item");
     // console.log(userDiv);
+// textContianer.querySelector(".title").textContent= element
+// console.log(textContianer)
+
+    userDiv.append(textContianer)
     userDiv.innerHTML = `
-    <button 
-    id="accordion-button" aria-expanded="false" class="title-wrapper"  data-loaded="false">
-    <span class="accordion-title"
-    ><p class="accordion-title-match">
-    ${element}
-    </p>
-    <div class="scores">
-    <div class="score">10</div>
-    <div  class="score" >15</div>
-    </div>
-    </span>
-    <input type="checkbox" value="${element}" name="${element}" />
+    <div class="item">
+      <p class="title">${element}</p>
+      <div class="scores">
+        <div class="score">10</div>
+        <div  class="score" >15</div>
+        </div>
+        <input type="checkbox" value="${element}" name="${element}" />
 
-
-    </button>
-    <div class="accordion-content">
-    <ul class="accordion-content--list">
-    </ul>
     </div>
     `;
 
@@ -49,7 +47,7 @@ textSubmit.addEventListener("click", (e) => {
 
 
 });
-
+// Second Toggle Start
 function secondToggleAccordion(target) {
   const name = target.querySelector(".accordion-title-match").innerText;
   console.log(name);
@@ -87,19 +85,23 @@ function secondToggleAccordion(target) {
     target.setAttribute("aria-expanded", false);
   }
 }
+// Second Toggle ENDS
+// textDiv.addEventListener("click", (e) => {
+//   let target = e.target;
 
-textDiv.addEventListener("click", (e) => {
-  let target = e.target;
+//   while (target != this) {
+//     if (target.tagName == "BUTTON") {
+//       // secondToggleAccordion(target);
+//       //   toggleAccordion()
+//       return;
+//     }
+//     target = target.parentNode;
+//   }
+// });
 
-  while (target != this) {
-    if (target.tagName == "BUTTON") {
-      secondToggleAccordion(target);
-      //   toggleAccordion()
-      return;
-    }
-    target = target.parentNode;
-  }
-});
+
+
+
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -107,17 +109,34 @@ form.addEventListener("submit", (e) => {
   let res = Object.fromEntries(data);
   let temp = Object.keys(res);
 
-  console.log(data, res, temp)
-  temp.forEach((key) => {
+  const card = template.content.cloneNode(true)
+  console.log( temp)
+  temp.forEach((key,idx) => {
+    temp[idx] = apiResponse[idx] ??temp[idx]
+    const resText = document.createElement('div')
+    resText.innerHTML = `
+    <div class="item">
+    <p class="title">${apiResponse[idx]}</p>
+    <div class="scores">
+      <div class="score">10</div>
+      <div  class="score" >15</div>
+      </div>
+      <input type="checkbox" value="${key}" name="${key}" />
+
+  </div>
+    `;
+    textDump.append(resText)
     console.log(key)
     let index = textArray.indexOf(key);
     console.log(index);
     textArray[index] = res[key];
   });
-  console.log(textArray);
+  // console.log(textArray);
 let newParagraph =""
+
+
 for(let i =0 ; i<textArray.length;i++){
-  newParagraph = newParagraph+ textArray[i]
+  newParagraph = newParagraph+ temp[i]?? textArray[i]
 }
   final__form.innerHTML =`
   <textarea name="" id="verifybox" cols="30" rows="10" placeholder="Write or Paste" class="textarea" disabled   >
@@ -128,6 +147,9 @@ for(let i =0 ; i<textArray.length;i++){
   `
 
 });
+
+
+
 
 avatatBtn.addEventListener("click", () => {
   const menu = document.querySelector(".avatar__menu");
